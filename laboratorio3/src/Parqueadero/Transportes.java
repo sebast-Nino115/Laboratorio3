5c5a1e631093e0b73e0e8a538fccc661834fe299
Nicolas Medina <u1803780@unimilitar.edu.co>
2020-10-03 20:05:56 -0500

    Transportes

--- laboratorio3/src/Parqueadero/Transportes.java
+++ laboratorio3/src/Parqueadero/Transportes.java
@@ -4,6 +4,7 @@ package Parqueadero;
 import java.util.Date;
 
 public class Transportes {
+    public String nombre;
     public String placa;
     public Date fechaingreso;
     public Date fechasalida;
